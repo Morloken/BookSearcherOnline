bd4e968158fe1476f8f3cb76227785c2e439d841
Morlok <nikitaillarionov2018@gmail.com>
2024-08-06 14:32:28 +0300

    installing dotenv, creating .env for DB, writing first code for app.js(main file of backend server config)

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bookRoutes = require('./routes/books');
+const favoriteRoutes = require('./routes/favorites');
+
+dotenv.config(); 
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected to BookSearchDB'))
+  .catch(err => console.log(err));
+
+app.use('/api/books', bookRoutes);
+app.use('/api/favorites', favoriteRoutes);
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
