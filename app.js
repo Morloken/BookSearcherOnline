52bbce48ed0ba77e638d483825695e410c7eb706
Morlok <nikitaillarionov2018@gmail.com>
2024-08-06 21:35:40 +0300

    added db from Atlas

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const bookRoutes = require('./routes/books');
-const favoriteRoutes = require('./routes/favorites');
-
-
-
-dotenv.config(); 
+const bookRoutes = require('./routes/bookRoutes'); 
+const favoriteRoutes = require('./routes/favoriteRoutes'); 
 
 const app = express();
+
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB connected to BookSearchDB'))
-  .catch(err => console.log(err));
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected to BookSearchDB');
 
+    
+    app.use('/api/books', bookRoutes);
+    app.use('/api/favorites', favoriteRoutes);
 
-app.use('/api/books', bookRoutes);
-app.use('/api/favorites', favoriteRoutes);
+    app.use(express.static('public'));
 
-app.use(express.static('public'));
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  } catch (err) {
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1); // Exit the process with an error code
+  }
+}
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
+startServer();
