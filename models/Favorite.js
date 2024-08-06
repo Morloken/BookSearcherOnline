98b629587499e908462db25156107e4d1d546ce5
Morlok <nikitaillarionov2018@gmail.com>
2024-08-06 19:47:29 +0300

    adding more parameters for Fav model, for more info abt books

--- models/Favorite.js
+++ models/Favorite.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const FavoriteSchema = new Schema({
+  userId: { type: String, required: true },
   isbn: { type: String, required: true },
-  userId: { type: String, required: true }
+  title: { type: String, required: true }, // Додано
+  thumbnail: { type: String }, // Додано
+  authors: { type: [String] }, // Додано
+  description: { type: String } // Додано
 });
 
 module.exports = mongoose.model('Favorite', FavoriteSchema);
+
